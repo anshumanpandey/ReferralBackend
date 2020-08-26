2f5b6ddc381d09e1bb944da2e4d4270be8c66c96
leo <leo_9_t@hotmail.com>
2020-08-26 10:10:41 -0400

    ADDED validation

--- src/routes/customer.route.ts
+++ src/routes/customer.route.ts
@@ -5,10 +5,13 @@ var guard = require('express-jwt-permissions')({
 })
 import asyncHandler from "express-async-handler"
 import { CustomerModel } from '../models/customer.model';
+import { ApiError } from '../utils/ApiError';
 
 export const customerRoutes = express();
 
 
 customerRoutes.post('/', asyncHandler(async (req, res) => {
+  const customer = await CustomerModel.findOne({ where: { id: req.body.id }})
+  if (customer) throw new ApiError("Customer already exist")
   res.send(await CustomerModel.create(req.body));
 }));
